3675e9949cc9201c851e70172725a2d6e6b24efc
jcopps <jeffry.copps@tivo.com>
2019-02-05 16:33:44 +0530

    Fixes

--- helpers/filter_training_data.py
+++ helpers/filter_training_data.py
@@ -1,15 +1,14 @@
-
 import os
 import json
 import codecs
 
-input_file = 'training_data_eng.json'
+input_file = 'training_data_eng.subintent.json'
 data = json.load(codecs.open(input_file, 'r', 'utf-8'))
 out_data = {"timestamp": 1549361454, "test_cases":{}}
 tcss = out_data['test_cases']
 for conv_id, conv in data.get('test_cases', {}).iteritems():
-    if conv[0]["sitedomain"] == "DomainName":
-	break 
+    if conv[0].get("sitedomain",'') == "Rovi-MusicSpace-en":
+	continue
     tcss[conv_id] = conv 
 
 out_file = ''.join([input_file,'filtered'])
